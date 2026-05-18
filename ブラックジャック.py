70cf4b83b386d5ae250d2f1c6ff971c996d94d3c
rei-by01 <madkzeon0823@gmail.com>
2026-05-18 17:12:58 +0900

    Add files via upload
    update

--- ブラックジャック.py
+++ ブラックジャック.py
@@ -1,5 +1,6 @@
 # ブラックジャック
 import random
+import time
 
 # 0 ゲームループ
 while True:
@@ -37,6 +38,15 @@ while True:
         return card
     
 
+    # テキストの時間を待つ処理
+    USE_EFFECT = True
+
+    def show(text, wait=2):
+        print(text)
+        if USE_EFFECT:
+            time.sleep(wait)
+    
+
     # --- ゲーム部分 ---
     # J Q K A の点数変換
     def calc(i):
@@ -54,6 +64,9 @@ while True:
         for c in i:
             total += c
         return total
+    
+
+     # --- 関数ここまで ---
 
 
     mark_list = ["♠", "☘", "♥", "♦"]
@@ -67,14 +80,14 @@ while True:
 
 
     # 1 ゲーム開始
+    show("ゲーム開始")
     # プレイヤーのカードドロー
     pcard_1 = draw_card(0, 1)
     drawncard_list.append(pcard_1)
 
     pcard_2 = p_append_card()
 
-    print("あなたのカードは" + str(pcard_1) +
-        "と" + str(pcard_2) + "です")
+    show(f"あなたのカードは {pcard_1} と {pcard_2} です")
 
     # ディーラーのカードドロー
     dcard_1 = draw_card(0, 1)
@@ -82,15 +95,14 @@ while True:
 
     dcard_2 = d_append_card()
 
-    print("ディーラーのカードは" + str(dcard_1) + 
-          "ともう一枚はまだ伏せられています")
+    show(f"ディーラーのカードは {dcard_1} ともう一枚はまだ伏せられています")
 
 
     # 2 ヒットとスタンド
     # 2.1 点数計算
     players_card.append(calc(pcard_1[1]))
     players_card.append(calc(pcard_2[1]))
-    print("現在のあなたの合計値は" , str(card_total(players_card)) , "です")
+    show(f"現在のあなたの合計値は {card_total(players_card)} です")
 
     # 2.2 ヒットとバースト判定
     while True:
@@ -100,21 +112,21 @@ while True:
             if ans == "y":
                 phit = ns_draw_card()
                 drawncard_list.append(phit)
-                print(str(phit) + "を引きました")
+                show(f"{phit} を引きました")
 
                 k = calc(phit[1])
                 players_card.append(k)
-                print("現在のあなたの合計値は" + str(card_total(players_card)) + "です")
+                show(f"現在のあなたの合計値は {card_total(players_card)} です")
                 continue
-            else: print("スタンドしました")
+            else: show("スタンドしました")
             break
 
         elif card_total(players_card) > 21:
             res = "lose"
-            print("バースト あなたの負け")
+            show("バースト あなたの負け")
             break
         elif card_total(players_card) == 21:
-            print("引き分け以上確定！ 自動的にディーラーのターンに移行します")
+            show("引き分け以上確定！ 自動的にディーラーのターンに移行します")
             break
         
 
@@ -122,8 +134,7 @@ while True:
     # 3.1 カードオープン + 点数計算
     dealer_card.append(calc(dcard_1[1]))
     dealer_card.append(calc(dcard_2[1]))
-    print("ディーラーのカードは", str(dcard_1) + "と" + str(dcard_2) + "で、合計値は" 
-        + str(card_total(dealer_card)) + "です")
+    show(f"ディーラーのカードは {dcard_1} と {dcard_2} で、合計値は {card_total(dealer_card)} です")
 
     # 3.2 プレイヤーがバーストしていたらディーラーはスキップ→結果表示へ
     if res == "lose":
@@ -134,35 +145,33 @@ while True:
             if card_total(dealer_card) < card_total(players_card):
                 dhit = ns_draw_card()
                 drawncard_list.append(dhit)
-                print("ディーラーは" + str(dhit) + "を引きました")
+                show(f"ディーラーは {dhit} を引きました")
 
                 k = calc(dhit[1])
                 dealer_card.append(k)
-                print("現在のディーラーの合計値は" 
-                      + str(card_total(dealer_card)) + "です")
+                show(f"現在のディーラーの合計値は {card_total(dealer_card)} です")
 
                 # バースト判定
                 if card_total(dealer_card) < 21:
                      continue
                 elif card_total(dealer_card) >= 22:
-                    print("ディーラーがバーストしました あなたの勝ち")
+                    show("ディーラーがバーストしました あなたの勝ち")
                     res = "win"
                     break
                 elif card_total(dealer_card) == 21:
                     pass
 
             elif card_total(dealer_card) > card_total(players_card):
-                print("あなたの負け")
+                show("あなたの負け")
                 break
             elif res == "win":
                 pass
             elif card_total(dealer_card) == card_total(players_card):
-                print("引き分け！")
+                show("引き分け！")
                 break
 
-    print("最終的な合計値", end=" ")
-    print("プレイヤー:" + str(card_total(players_card)) + 
-          " ディーラー:" + str(card_total(dealer_card)))
+    print("最終的な合計値 ", end="")
+    show(f"プレイヤー: {card_total(players_card)} ディーラー: {card_total(dealer_card)}")
     # print("引いたカードテスト" + str(drawncard_list))
 
     ques = input("もう一度遊びますか？ 遊ぶならyを入力してEnter " \
